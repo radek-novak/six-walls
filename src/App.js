b81ff3da38ad01bf13abbc28a59b67ebad8e4b68
Radek Novak <radek.novak.cze@gmail.com>
2017-07-02 16:26:46 +0200

    init

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import Canvas from './Canvas';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Canvas />
+      </div>
+    );
+  }
+}
+
+export default App;
